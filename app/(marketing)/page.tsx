d02afbf19973f2a37f0b3d735c5fc91e74fab254
soreyLong <794790374@qq.com>
2024-10-13 22:08:45 +0800

    solve: convex

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,11 @@
-import Heading from "./_components/heading";
-import { Heros } from "./_components/heros";
+import { Footer } from './_components/footer'
+import Heading from './_components/heading'
+import { Heroes } from './_components/heroes'
 
 const MarketingPage = () => {
   return (
-    <div className="min-h-full flex flex-col">
+    // INFO: 这里加了个dark:bg-[#1f1f1f]，否则只加在布局那里会有不同的颜色
+    <div className="min-h-full flex flex-col dark:bg-[#1f1f1f]">
       {/* md:justify-start：在中等及以上屏幕尺寸时，
       子元素在垂直方向上从顶部开始排列。 */}
       <div
@@ -12,10 +14,11 @@ const MarketingPage = () => {
           flex-1 px-6 pb-10"
       >
         <Heading></Heading>
-        <Heros></Heros>
+        <Heroes></Heroes>
       </div>
+      <Footer></Footer>
     </div>
-  );
-};
+  )
+}
 
-export default MarketingPage;
+export default MarketingPage
