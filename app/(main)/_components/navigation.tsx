9512627c9805359bd825674fe7a8c046e6855eda
soreyLong <794790374@qq.com>
2025-04-23 17:55:50 +0800

    feat: navigation component of documents page

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,18 +1,113 @@
-import { ChevronLeft } from 'lucide-react'
-import React from 'react'
+import { cn } from '@/lib/utils'
+import { ChevronsLeft, MenuIcon } from 'lucide-react'
+import { usePathname } from 'next/navigation'
+import React, { ElementRef, useEffect, useRef, useState } from 'react'
+import { useMediaQuery } from 'usehooks-ts'
 
 const Navigation = () => {
+  const pathname = usePathname()
+  const isMobile = useMediaQuery('(max-width: 768px')
+  const isResizingRef = useRef(false)
+  const sidebarRef = useRef<ElementRef<'aside'>>(null)
+  const navbarRef = useRef<ElementRef<'div'>>(null)
+  const [isResetting, setIsResetting] = useState(false)
+  const [isCollapsed, setIsCollapsed] = useState(false)
+
+  // 应对从网页端缩小到移动端时的情况
+  useEffect(() => {
+    if (isMobile) {
+      collapse()
+    } else {
+      resetWidth()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isMobile])
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isMobile, pathname])
+
+  const handleMouseDown = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    event.preventDefault()
+    event.stopPropagation()
+    isResizingRef.current = true
+    document.addEventListener('mousemove', handleMouseMove)
+    document.addEventListener('mouseup', handleMouseUp)
+  }
+
+  const handleMouseMove = (event: MouseEvent) => {
+    if (!isResizingRef.current) return
+    let newWidth = event.clientX
+    if (newWidth < 240) newWidth = 240
+    if (newWidth > 480) newWidth = 480
+    if (sidebarRef.current && navbarRef.current) {
+      sidebarRef.current.style.width = `${newWidth}px`
+      navbarRef.current.style.setProperty('left', `${newWidth}px`)
+      navbarRef.current.style.setProperty('width', `calc(100% - ${newWidth}px)`)
+    }
+  }
+
+  const handleMouseUp = () => {
+    isResizingRef.current = false
+    document.removeEventListener('mousemove', handleMouseMove)
+    document.removeEventListener('mouseup', handleMouseUp)
+  }
+
+  const resetWidth = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(false)
+      setIsResetting(true)
+
+      // 如果是移动端设备，侧边栏应该要占满整个屏幕，还要把顶部的 nav 给隐藏掉
+      sidebarRef.current.style.width = isMobile ? '100%' : '240px'
+      navbarRef.current.style.setProperty(
+        'width',
+        isMobile ? '0' : 'calc(100% - 240px)',
+      )
+      navbarRef.current.style.setProperty('left', isMobile ? '100%' : '240px')
+      setTimeout(() => setIsResetting(false), 300)
+    }
+  }
+
+  const collapse = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(true)
+      setIsResetting(true)
+      sidebarRef.current.style.width = '0'
+      // navbarRef.current.style.setProperty('width', '100%')
+      // navbarRef.current.style.setProperty('left', '0')
+      navbarRef.current.style.width = '100%'
+      navbarRef.current.style.left = '0'
+      
+      setTimeout(() => setIsResetting(false), 300)
+    }
+  }
+
   return (
     <>
+      {/* tailwindcss 可以定义一个组group/sidebar，组名为sidebar */}
       <aside
-        className="group/sidebar h-full bg-secondary overflow-y-auto
-          relative flex w-60 flex-col z-[99999]"
+        ref={sidebarRef}
+        className={cn(
+          'group/sidebar relative z-[99999] flex h-full w-60 flex-col overflow-y-auto bg-secondary',
+          isResetting && 'transition-all duration-300 ease-in-out',
+          isMobile && 'w-0',
+        )}
       >
         <div
           role="button"
-          className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition"
+          onClick={collapse}
+          className={cn(
+            'absolute right-2 top-3 h-6 w-6 rounded-sm text-muted-foreground opacity-0 transition hover:bg-neutral-300 group-hover/sidebar:opacity-100 dark:hover:bg-neutral-600',
+            isMobile && 'opacity-100',
+          )}
         >
-          <ChevronLeft className="h-6 w-6" />
+          <ChevronsLeft className="h-6 w-6" />
         </div>
         <div>
           <p>Action items</p>
@@ -22,11 +117,29 @@ const Navigation = () => {
         </div>
         {/* 纵向分隔符 */}
         <div
-          className="opacity-0 group-hover/sidebar:opacity-100
-          transition cursor-ew-resize absolute h-full w-1 
-          bg-primary/10 right-0 top-0"
+          onMouseDown={handleMouseDown}
+          onClick={resetWidth}
+          className="absolute right-0 top-0 h-full w-1 cursor-ew-resize bg-primary/10 opacity-0 transition group-hover/sidebar:opacity-100"
         />
       </aside>
+      <div
+        ref={navbarRef}
+        className={cn(
+          'absolute left-60 top-0 z-[99999] w-[calc(100%-240px)]',
+          isResetting && 'transition-all duration-300 ease-in-out',
+          isMobile && 'left-0 w-full',
+        )}
+      >
+        <nav className="w-full bg-transparent px-3 py-2">
+          {isCollapsed && (
+            <MenuIcon
+              role="button"
+              onClick={resetWidth}
+              className="h-6 w-6 text-muted-foreground"
+            />
+          )}
+        </nav>
+      </div>
     </>
   )
 }
